bf7923db4d747897c7c2611c41fffdc3f47a6c66
Mengzhen <gao.mengzhen94@gmail.com>
2017-02-25 18:49:19 -0500

    simplified Tour of Heros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+    <my-heroes></my-heroes>
+  `,
 })
 export class AppComponent  { name = 'Angular'; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroListComponent } from './heroes/hero-list.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ 
+    AppComponent,
+    HeroesComponent,
+    HeroListComponent
+   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
